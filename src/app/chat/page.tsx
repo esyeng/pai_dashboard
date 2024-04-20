5e0f79d10942a5d3e8fb4701aee81c6ffee9e0e7
Esme Y <61486616+esyeng@users.noreply.github.com>
2024-04-19 22:56:04 -0400

    meh

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+}
+
+const MyComponent: React.FC<Props> = () => {
+    return (
+        <div>
+            {/* meh */}
+        </div>
+    );
+};
+
+export default MyComponent;
