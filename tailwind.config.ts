7ef47c1c8b02746880be06c714307347519f7fe3
Esme Y <61486616+esyeng@users.noreply.github.com>
2024-04-22 00:09:54 -0400

    Add Radix UI packages and update global CSS colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+          backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+              "conic-gradient(from 90deg at 50% 50%, var(--tw-gradient-stops))",
+          },
+        },
+        colors: {
+            "sandy-brown": "#fc9f5bff",
+            "light-coral": "#ed7b84ff",
+            "vista-blue": "#72a1e5ff",
+            "tropical-indigo": "#9883e5ff",
+            "tea-green": "#c4f4c7ff",
+            "chocolate-cosmos": "#4c212aff",
+            "caribbean-current": "#0d5c63ff",
+            "mint": "#5fbb97ff",
+            "sunset": "#f2d0a4ff",
+            "ecru": "#a69658ff",
+            "ecru-2": "#d9b26fff",
+            "jasmine": "#fadf7fff",
+            "vanilla": "#f2e29fff",
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
