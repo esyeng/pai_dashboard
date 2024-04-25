cfb459c5c3481d766ebf6e41901f72217afc7345
esyeng <yenigun13@gmail.com>
2024-04-25 12:17:25 -0400

    Refactor page.tsx and api.ts to improve code readability and remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,56 @@
 import type { NextPage, Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "JasmynAI",
-    description: "Private personal AI dashboard",
+	title: "JasmynAI",
+	description: "Private personal AI dashboard",
 };
 import Image from "next/image";
 import { ChatWindow } from "./components/ChatWindow";
 import { Options } from "./components/OptionsPanel";
 
 const agents = [
-    { id: "jasmyn", name: "Jasmyn" },
-    { id: "coder", name: "Coder" },
-    { id: "fed", name: "F.E.D." },
-    { id: "tutor", name: "AI Tutor" },
-    { id: "ada", name: "Ada" },
-    { id: "sys_writer", name: "System Prompter" },
-    { id: "sys_writer_plus", name: "System Prompter+" }
+	{ id: "jasmyn", name: "Jasmyn" },
+	{ id: "coder", name: "Coder" },
+	{ id: "fed", name: "F.E.D." },
+	{ id: "tutor", name: "AI Tutor" },
+	{ id: "ada", name: "Ada" },
+	{ id: "sys_writer", name: "System Prompter" },
+	{ id: "sys_writer_plus", name: "System Prompter+" },
 ];
 
 const models = [
-    { id: "claude-3-opus-20240229", name: "Opus" },
-    { id: "claude-3-sonnet-20240229", name: "Sonnet" },
-    { id: "claude-3-haiku-20240307", name: "Haiku" },
+	{ id: "claude-3-opus-20240229", name: "Opus" },
+	{ id: "claude-3-sonnet-20240229", name: "Sonnet" },
+	{ id: "claude-3-haiku-20240307", name: "Haiku" },
 ];
 
 const Home: NextPage = () => {
-    return (
-        <div className="container mx-auto">
-            <main className="flex h-full flex-col items-center justify-between px-14 py-2 my-16">
-                <div className=" w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                    <div className="flex h-10 w-full items-end justify-center">
-                        <div className="z-10 bg-ultra-violet text-white">
-                            <h1 className="flex cursor-crosshair place-items-center gap-2 p-8 w-full text-[#9de6ca] text-xl py-2 pr-8 rounded leading-tight hover:scale-105">
-                                {"<"} jasmyn.ai {"/>"}
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-                <Options agents={agents} models={models} />
+	return (
+		<div className="container mx-auto">
+			<main className="flex h-full flex-col items-center justify-between px-0 py-2 my-16 sm:px-8">
+				<div className=" w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+					<div className="flex h-10 w-full items-end justify-center">
+						<div className="z-10 bg-ultra-violet text-white">
+							<h1 className="flex cursor-crosshair place-items-center gap-2 p-8 w-full text-[#9de6ca] text-xl py-2 pr-8 rounded leading-tight hover:scale-105">
+								{"<"} jasmyn.ai {"/>"}
+							</h1>
+						</div>
+					</div>
+				</div>
+				<Options agents={agents} models={models} />
 
-                <ChatWindow />
+				<ChatWindow />
 
-                <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left"></div>
-            </main>
-            <footer className="py-4 text-center">
-                <p className="text-mint">
-                    &copy; {new Date().getFullYear()} Esmé Keats. All rights
-                    reserved.
-                </p>
-            </footer>
-        </div>
-    );
+				<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left"></div>
+			</main>
+			<footer className="py-4 text-center">
+				<p className="text-mint">
+					&copy; {new Date().getFullYear()} Esmé Keats. All rights
+					reserved.
+				</p>
+			</footer>
+		</div>
+	);
 };
 
 export default Home;
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,46 +1,46 @@
-const BASE = "https://jasmyn-cb3idkum5a-uc.a.run.app"
+const BASE = "https://jasmyn-cb3idkum5a-uc.a.run.app";
 const API_URL = `${BASE}/claude`;
-console.log(process.env.NODE_ENV === "production");
-console.log(process.env.API_URL);
+// console.log(process.env.NODE_ENV === "production");
+// console.log(process.env.API_URL);
 
 interface ChatRequestParams {
-    max_tokens: number;
-    model: string;
-    temperature: number;
-    agent_id: string;
-    messages: { role: string; content: string }[];
+	max_tokens: number;
+	model: string;
+	temperature: number;
+	agent_id: string;
+	messages: { role: string; content: string }[];
 }
 
 interface ModelResponse {
-    response?:
-    | any
-    | {
-        text?: string;
-        response?: string | JSON;
-    };
-    text?: string;
+	response?:
+		| any
+		| {
+				text?: string;
+				response?: string | JSON;
+		  };
+	text?: string;
 }
 
 export const queryModel = async (
-    params: ChatRequestParams
+	params: ChatRequestParams
 ): Promise<ModelResponse> => {
-    try {
-        const response = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(params),
-        });
+	try {
+		const response = await fetch(API_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(params),
+		});
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
 
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error sending chat request:", error);
-        throw error;
-    }
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error("Error sending chat request:", error);
+		throw error;
+	}
 };
