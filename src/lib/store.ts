dbf6da96490b671ad641b45e5eef76883950639c
Esmé Keats <esmekeats@pm.me>
2024-06-22 14:40:57 -0400

    update model names

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,18 @@
 export const agents = [
-	{ id: "jasmyn", name: "Jasmyn" },
-	{ id: "coder", name: "Coder" },
-	{ id: "fed", name: "F.E.D." },
-	{ id: "tutor", name: "AI Tutor" },
-	{ id: "ada", name: "Ada" },
-	{ id: "sys_writer", name: "System Prompter" },
-	{ id: "sys_writer_plus", name: "System Prompter+" },
-	{ id: "dm_helper", name: "D&D: DM Helper" },
-	{ id: "player_helper", name: "D&D: Player Helper" },
+    { id: "jasmyn", name: "Jasmyn" },
+    { id: "coder", name: "Coder" },
+    { id: "fed", name: "F.E.D." },
+    { id: "tutor", name: "AI Tutor" },
+    { id: "ada", name: "Ada" },
+    { id: "sys_writer", name: "System Prompter" },
+    { id: "sys_writer_plus", name: "System Prompter+" },
+    { id: "dm_helper", name: "D&D: DM Helper" },
+    { id: "player_helper", name: "D&D: Player Helper" },
 ];
 
 export const models = [
-	{ id: "claude-3-opus-20240229", name: "Opus" },
-	{ id: "claude-3-sonnet-20240229", name: "Sonnet" },
-	{ id: "claude-3-haiku-20240307", name: "Haiku" },
+    { id: "claude-3-5-sonnet-20240620", name: "Claude 3.5 Sonnet" },
+    { id: "claude-3-opus-20240229", name: "Claude 3 Opus" },
+    { id: "claude-3-sonnet-20240229", name: "Claude 3 Sonnet" },
+    { id: "claude-3-haiku-20240307", name: "Claude 3 Haiku" },
 ];
