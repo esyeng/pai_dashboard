bf52480f1b758ee276dddf8d36cfe955163c1361
Esmé Keats <esmekeats@pm.me>
2024-09-19 18:23:30 -0400

    Gradient removal & option readability fix

--- src/app/components/OptionsPanel.tsx
+++ src/app/components/OptionsPanel.tsx
@@ -14,7 +14,7 @@ export const Options: React.FC<OptionsProps> = ({ agents, models }) => {
 		<div className="flex  justify-center w-full sm:flex-col">
 			<div className="mb-4 flex flex-1 flex-col items-center justify-center my-2 lg:flex-row p-2 rounded-3xl bg-gradient-to-b from-[#4ce6ab2d] to-[#0ea46a3b] shadow-xl">
 				<div className="flex justify-center items-center px-2 my-1 mx-4">
-					<span className="text-caribbean-current text-sm py-2 pr-2 rounded leading-tight">
+					<span className="text-[#9de6ca] text-lg py-2 pr-2 rounded leading-tight">
 						Model:{" "}
 					</span>
 					<span className="text-md text-caribbean-current my-4 mr-2">
@@ -23,7 +23,7 @@ export const Options: React.FC<OptionsProps> = ({ agents, models }) => {
 					<AgentDropdown agents={models} mode="model" />
 				</div>
 				<div className="flex justify-center items-center px-2 my-1 mx-4">
-					<span className="text-caribbean-current text-sm py-2 pr-2 rounded leading-tight">
+					<span className="text-[#9de6ca] text-lg py-2 pr-2 rounded leading-tight">
 						Agent:{" "}
 					</span>
 					<span className="text-md text-caribbean-current my-4 mr-2">
