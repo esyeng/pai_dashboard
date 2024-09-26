7503d38a05a179b4d23091205a621803ffca5860
Esmé Keats <esmekeats@pm.me>
2024-09-26 19:44:46 -0400

    Update notes panel to dynamically clear

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -5,90 +5,119 @@ import { ChatWindow } from "./ChatWindow";
 import { Options } from "./OptionsPanel";
 import { Sidebar } from "./Sidebar";
 import { useChat } from "@/contexts/ChatContext";
+import { SignOutButton } from "@clerk/nextjs";
+import { SignInOrOut } from "./account/SignInOrOut";
 
-import { agents, models } from "../../lib/store";
+// import { agents, models } from "../../lib/store";
+// import { fetchAssistants, fetchModels } from "../../lib/api";
 import { useAuth } from "@clerk/nextjs";
 
-const getAgents = () => {
-    console.log("get agents called");
 
-}
 
 export const MainContent: React.FC = () => {
-	const [sideOnBottom, setSideOnBottom] = useState(false);
-	const { isLoaded, userId, sessionId, getToken } = useAuth();
-	const { threads, user, setToken, agentId, modelId } = useChat();
+    const [sideOnBottom, setSideOnBottom] = useState(false);
+    const { isLoaded, userId, sessionId, getToken } = useAuth();
+    const { threads, agents, models, user, setToken, agentId, modelId } = useChat();
 
-	useEffect(() => {
-		const handleResize = () => {
-			setSideOnBottom(window.innerWidth < 768);
-		};
+    useEffect(() => {
+        const handleResize = () => {
+            setSideOnBottom(window.innerWidth < 768);
+        };
 
-		window.addEventListener("resize", handleResize);
+        window.addEventListener("resize", handleResize);
 
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
-	useEffect(() => {
-		if (isLoaded) {
-			setToken(getToken());
-		}
-		console.log(
-			`data present in MainContent after isLoaded is evaluated: first, is it?: ${isLoaded}. Soo the userId is: ${userId} and the sessionId is: ${sessionId} and the token is: ${getToken()}. user?: ${user} and agentId?: ${agentId} and modelId?: ${modelId} and threads?: ${threads} and agents?: ${agents} and models?: ${models} and sideOnBottom?: ${sideOnBottom} and setSideOnBottom?: ${setSideOnBottom}`
-		);
-	}, [isLoaded]);
+    useEffect(() => {
+        if (isLoaded) {
+            setToken(getToken());
+        }
+        // console.log(
+        //     `data present in MainContent after isLoaded is evaluated: first, is it?: ${isLoaded}. Soo the userId is: ${userId} and the sessionId is: ${sessionId} and the token is: ${getToken()}. user?: ${user} and agentId?: ${agentId} and modelId?: ${modelId} and threads?: ${threads} and agents?: ${agents} and models?: ${models} and sideOnBottom?: ${sideOnBottom} and setSideOnBottom?: ${setSideOnBottom}`
+        // );
+    }, [isLoaded]);
 
-	return (
-		<div className="flex justify-between w-full max-md:flex-col">
-			{!sideOnBottom ? <Sidebar /> : null}
-			<div className="flex flex-col flex-1 px-4 items-center justify-between max-w-full overflow-x-auto">
-            <button
-            onClick={getAgents}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
-        >
-            Get Agents
-        </button>
-				<div className=" w-full items-center justify-between font-mono text-sm lg:flex">
-					<div className="flex h-10 w-full items-end justify-center">
-						<div className="z-10 bg-ultra-violet text-white">
-							<h1 className="flex cursor-crosshair place-items-center gap-2 p-8 w-full text-[#9de6ca] text-xl py-2 pr-8 rounded leading-tight hover:scale-105">
-								{"<"} jasmyn.ai {"/>"}
-							</h1>
-						</div>
-					</div>
-				</div>
-				<Options agents={agents} models={models} />
-				<ChatWindow />
-				{sideOnBottom ? <Sidebar /> : null}
-			</div>
-		</div>
-	);
+
+
+    return (
+        <div className="flex justify-between w-full max-md:flex-col">
+            {!sideOnBottom ? <Sidebar /> : null}
+            <div className="flex flex-col flex-1 px-4 items-center justify-between max-w-full overflow-x-auto">
+                <SignInOrOut />
+                {/* <button
+                    onClick={getAgents}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out"
+                >
+                    Get Agents
+                </button> */}
+                <div className=" w-full items-center justify-between font-mono text-sm lg:flex">
+                    <div className="flex h-10 w-full items-end justify-center">
+                        <div className="z-10 bg-ultra-violet text-white">
+                            <h1 className="flex cursor-crosshair place-items-center gap-2 p-8 w-full text-[#9de6ca] text-xl py-2 pr-8 rounded leading-tight hover:scale-105">
+                                {"<"} jasmyn.ai {"/>"}
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+                {(isLoaded) ? (
+                    <Options agents={agents} models={models} />) : (
+                    <div className="h-12">
+                        <div className="flex items-center justify-center bg-gray-200 rounded-lg p-4">
+                            <svg
+                                className="w-5 h-5 text-gray-600 animate-spin"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    className="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    strokeWidth="4"
+                                ></circle>
+                                <path
+                                    className="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                ></path>
+                            </svg>
+                        </div>
+                    </div>
+                )}
+                <ChatWindow />
+                {sideOnBottom ? <Sidebar /> : null}
+            </div>
+        </div>
+    );
 };
 
 /**
  * const [sideOnBottom, setSideOnBottom] = useState(false);
  *
  * div className="flex justify-between w-full max-sm:flex-col">
-					{!sideOnBottom ? (
-						<Sidebar setSideOnBottom={setSideOnBottom} />
-					) : null}
-					<div className="flex flex-col flex-1 px-4 items-center justify-between ">
-						<div className=" w-full items-center justify-between font-mono text-sm lg:flex">
-							<div className="flex h-10 w-full items-end justify-center">
-								<div className="z-10 bg-ultra-violet text-white">
-									<h1 className="flex cursor-crosshair place-items-center gap-2 p-8 w-full text-[#9de6ca] text-xl py-2 pr-8 rounded leading-tight hover:scale-105">
-										{"<"} jasmyn.ai {"/>"}
-									</h1>
-								</div>
-							</div>
-						</div>
-						<Options agents={agents} models={models} />
-						<ChatWindow />
-						{sideOnBottom ? <Sidebar /> : null}
-					</div>
-				</div>
+                    {!sideOnBottom ? (
+                        <Sidebar setSideOnBottom={setSideOnBottom} />
+                    ) : null}
+                    <div className="flex flex-col flex-1 px-4 items-center justify-between ">
+                        <div className=" w-full items-center justify-between font-mono text-sm lg:flex">
+                            <div className="flex h-10 w-full items-end justify-center">
+                                <div className="z-10 bg-ultra-violet text-white">
+                                    <h1 className="flex cursor-crosshair place-items-center gap-2 p-8 w-full text-[#9de6ca] text-xl py-2 pr-8 rounded leading-tight hover:scale-105">
+                                        {"<"} jasmyn.ai {"/>"}
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                        <Options agents={agents} models={models} />
+                        <ChatWindow />
+                        {sideOnBottom ? <Sidebar /> : null}
+                    </div>
+                </div>
 
-				<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left"></div>
+                <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left"></div>
  */
--- src/app/components/NotesPanel.tsx
+++ src/app/components/NotesPanel.tsx
@@ -1,37 +1,74 @@
 // src/app/components/NotesPanel.tsx
 import React, { useState, useEffect } from "react";
+import { useChat } from "../../contexts/ChatContext";
 
 const NotesPanel: React.FC = () => {
-	const [notes, setNotes] = useState("");
+    const [notes, setNotes] = useState("");
+    const { loadComplete, user } = useChat();
 
-	useEffect(() => {
-		const storedNotes = localStorage.getItem("notes");
-		if (storedNotes) {
-			setNotes(storedNotes);
-		}
-	}, []);
+    // useEffect(() => {
+    //     if (user && user.id) {
+    //         resetNotesOnUserChange(user.id);
+    //     }
+    //     const storedNotes = localStorage.getItem("notes");
+    //     if (storedNotes) {
+    //         console.log("last note user is current user")
+    //         setNotes(storedNotes);
+    //     }
+    // }, [loadComplete]);
 
-	const handleNotesChange = (
-		event: React.ChangeEvent<HTMLTextAreaElement>
-	) => {
-		const newNotes = event.target.value;
-		setNotes(newNotes);
-		localStorage.setItem("notes", newNotes);
-	};
+    useEffect(() => {
+        const storedNotes = localStorage.getItem("notes");
+        if (storedNotes) {
+            console.log("last note user is current user")
+            setNotes(storedNotes);
+        }
+    }, []);
 
-	return (
-		<div className="flex flex-col mb-4 flex-1 w-full h-full">
-			<h2 className="flex-1 text-lg text-[#cfedf0] font-bold border-b-1  rounded-tr-lg p-4 bg-transparent border-mint border-2 ">
-				Notes
-			</h2>
-			<textarea
-				className="max-h-[36rem] min-h-24 border-mint border-2 border-t-1 rounded-b-xl shadow-m bg-[#fff1c000] text-[#cfedf0] border-slate-300 py-2 pl-9 pr-3 placeholder:text-[#85d7de] transition ease-in-out hover:bg-mint/20 focus:bg-mint/20 focus:border-sky-500 focus:ring-sky-500  focus:ring-offset-1 focus:ring-offset-[#5bdde8] duration-200 w-full  border-gray-300 focus:ring-2 focus:ring-blue-500 resize-y"
-				value={notes}
-				onChange={handleNotesChange}
-				placeholder="Write your notes here..."
-			/>
-		</div>
-	);
+    const handleNotesChange = (
+        event: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+        const newNotes = event.target.value;
+        setNotes(newNotes);
+        localStorage.setItem("notes", newNotes);
+    };
+
+
+    return (
+        <div className="flex flex-col mb-4 flex-1 w-full h-full">
+            <h2 className="flex-1 text-lg text-[#cfedf0] font-bold border-b-1  rounded-tr-lg p-4 bg-transparent border-mint border-2 ">
+                Notes
+            </h2>
+            <textarea
+                className="max-h-[36rem] min-h-24 border-mint border-2 border-t-1 rounded-b-xl shadow-m bg-[#fff1c000] text-[#cfedf0] border-slate-300 py-2 pl-9 pr-3 placeholder:text-[#85d7de] transition ease-in-out hover:bg-mint/20 focus:bg-mint/20 focus:border-sky-500 focus:ring-sky-500  focus:ring-offset-1 focus:ring-offset-[#5bdde8] duration-200 w-full  border-gray-300 focus:ring-2 focus:ring-blue-500 resize-y"
+                value={notes}
+                onChange={handleNotesChange}
+                placeholder="Write your notes here..."
+            />
+        </div>
+    );
 };
 
 export default NotesPanel;
+
+
+
+/**
+ * if (user && user.id) {
+            console.log(`checking notes for ${user.firstName}`)
+            if (!lastNoteUser) {
+                console.log(`${user.firstName} has not had id logged for notepad`)
+                localStorage.setItem("note user id", user.id);
+            } else if (storedNotes && lastNoteUser !== user.id) {
+                console.log(`${user.firstName}'s user id does not match the locally stored note user id, clearing notes and setting note user to ${user.firstName}'s id.`)
+                setNotes("")
+                localStorage.setItem("note user id", user.id)
+            }
+            if (storedNotes && lastNoteUser === user.id) {
+                console.log("last note user is current user")
+                setNotes(storedNotes);
+            }
+        } else {
+            console.log("no user and user id in NotesPanel")
+        }
+ */
